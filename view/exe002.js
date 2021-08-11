22f425e37aa42ea5ded4849bdbbb63beee614395
Gabriel-RLopes <grgabrielrochalopes012@gmail.com>
2021-08-10 21:50:38 -0300

    feito separação do arquivo js do html

---
+++ view/exe002.js
@@ -0,0 +1,9 @@
+function Enviar(){
+   var nome = document.getElementById("name");
+   var idade = document.getElementById("age");
+   var peso = document.getElementById("peso");
+
+   window.alert(nome.value);
+   window.alert(idade.value);
+   window.alert(peso.value);
+}
